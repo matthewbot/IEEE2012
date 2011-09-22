93a620ca42901c3b0f524abe0102a721635c389a
Forrest Voight <forrest@forre.st>
2011-09-21 23:51:51 -0400

    Retry initial connection, pulsing DTR. Also, retry entire programming procedure in case of communications errors. Seems competely reliable.

--- chip45boot2.py
+++ chip45boot2.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import time
+import traceback
 
 import serial
 
@@ -31,7 +32,7 @@ class UnexpectedError(Exception):
 
 def expect(*choices):
     pos = 0
-    res = ""
+    res = ''
     while True:
         if len(choices) == 1 and pos == len(choices[0]):
             return choices[0]
@@ -42,51 +43,61 @@ def expect(*choices):
         choices = [x for x in choices if x[pos] == c]
         pos += 1
 
-s.write('R') # might reset the processor
-
-print 'Attempting communication... Reset processor if it gets stuck here. (Possibly more than once!)'
-
-data = ''
 while True:
-    s.write('U'*10)
-    time.sleep(.1)
-    data = data[-100:] + s.read(s.inWaiting())
-    #time.sleep(.1)
-    #print repr(data)
-    if data.endswith('c45b2 v2.9C\n\r>\x11'):
-        break
-
-print 'Communication established!'
-
-
-# test communication
-s.write('\n')
-expect('\x13-\n\r>\x11')
-
-# start programming
-if args.eeprom:
-    s.write('pe\n')
-    expect('\x13pe+\n\r\x11')
-else:
-    s.write('pf\n')
-    expect('\x13pf+\n\r\x11')
-
-# send data, displaying progress
-for i, line in enumerate(hex_file_lines):
-    s.write(line)
-    expect('\x13.')
-    if i == len(hex_file_lines) - 1:
-        e = '+\n\r>\x11'
-    else:
-        e = '\x11'
-    sys.stdout.write('.')
-    if expect(e, '*' + e).startswith('*'):
-        sys.stdout.write('*')
-    sys.stdout.flush()
-sys.stdout.write('\n')
-
-# start program
-s.write('g\n')
-expect('\x13g+\n\r\x11')
-
-print 'Success!'
+    try:
+        print 'Attempting communication...'
+        
+        while True:
+            data = ''
+            s.setDTR(True)
+            time.sleep(.1)
+            s.setDTR(False)
+            for i in xrange(5):
+                time.sleep(.025)
+                s.write('U'*10)
+                time.sleep(.025)
+                data += s.read(s.inWaiting())
+                if data.endswith('c45b2 v2.9C\n\r>\x11'):
+                    break
+            else:
+                continue
+            break
+        
+        print 'Communication established!'
+        
+        # test communication
+        s.write('\n')
+        expect('\x13-\n\r>\x11')
+        
+        # start programming
+        if args.eeprom:
+            s.write('pe\n')
+            expect('\x13pe+\n\r\x11')
+        else:
+            s.write('pf\n')
+            expect('\x13pf+\n\r\x11')
+        
+        # send data, displaying progress
+        for i, line in enumerate(hex_file_lines):
+            s.write(line)
+            expect('\x13.')
+            if i == len(hex_file_lines) - 1:
+                e = '+\n\r>\x11'
+            else:
+                e = '\x11'
+            sys.stdout.write('.')
+            if expect(e, '*' + e).startswith('*'):
+                sys.stdout.write('*')
+            sys.stdout.flush()
+        sys.stdout.write('\n')
+        
+        # start program
+        s.write('g\n')
+        expect('\x13g+\n\r\x11')
+        
+        print 'Success!'
+    except UnexpectedError:
+        traceback.print_exc()
+        print 'Communications error, retrying...'
+        continue
+    break
