57b49bd091a4e542a449c0b3682bd2571854b527
Forrest Voight <forrest.voight@gmail.com>
2011-10-19 16:54:18 -0400

    Fixed bug in util.h where util_cli_lo disabled all interrupts

--- FlamingArrow/util.h
+++ FlamingArrow/util.h
@@ -4,7 +4,7 @@
 #include <avr/io.h>
 
 // low priority interrupt control, leaves medium and high interrupts active
-inline void util_cli_lo() { PMIC.CTRL &= PMIC_LOLVLEN_bm; }
+inline void util_cli_lo() { PMIC.CTRL &= ~PMIC_LOLVLEN_bm; }
 inline void util_sei_lo() { PMIC.CTRL |= PMIC_LOLVLEN_bm; }
 
 #endif /* UTIL_H_ */
